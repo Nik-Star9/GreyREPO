a2d35006c6a396520ba73f6da8228ff2f5f3b063
Nik-Star9 <nikhileshlonkar1@gmail.com>
2025-02-25 19:29:14 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RAILWAY_PUBLIC_URL', 'https://greyrepo-greyriversystem.up.railway.app/'),
   app: {
     keys: env.array('APP_KEYS'),
   },
