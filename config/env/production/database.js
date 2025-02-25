d22d9f839f777fb7548b552df9b2c1def7dcfa55
Nik-Star9 <nikhileshlonkar1@gmail.com>
2025-02-26 00:00:36 +0530

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,9 +8,9 @@ module.exports = ({ env }) => {
           ? {
               host: env('DATABASE_HOST'),
               port: env.int('DATABASE_PORT', 5432),
-              database: env('DATABASE_NAME', 'your-db-name'),
-              user: env('DATABASE_USERNAME', 'your-db-user'),
-              password: env('DATABASE_PASSWORD', 'your-db-password'),
+              database: env('DATABASE_NAME'),
+              user: env('DATABASE_USERNAME'),
+              password: env('DATABASE_PASSWORD'),
               ssl: env.bool('DATABASE_SSL', true),
             }
           : {
